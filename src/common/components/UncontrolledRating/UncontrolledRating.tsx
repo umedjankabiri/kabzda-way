f7ba8a480adb4014d6c1a12f55012d72d73a5a7e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 23:28:21 +0300

    has been created UncontrolledRating component. The component not have a type, added useState() hook for monitoring state of component. Implemented attributes "selected" and "onClick" for UncontrolledStar component in the Rating component

---
+++ src/common/components/UncontrolledRating/UncontrolledRating.tsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import {UncontrolledStar} from "common/components/UncontrolledStar/UncontrolledStar.tsx";
+
+export const UncontrolledRating = () => {
+    console.log("UncontrolledRating rendering")
+    const [value, setValue] = useState(0);
+
+    return (
+        <div>
+            <UncontrolledStar selected={value > 0} onClick={()=> setValue(1)}/>
+            <UncontrolledStar selected={value > 1} onClick={()=> setValue(2)}/>
+            <UncontrolledStar selected={value > 2} onClick={()=> setValue(3)}/>
+            <UncontrolledStar selected={value > 3} onClick={()=> setValue(4)}/>
+            <UncontrolledStar selected={value > 4} onClick={()=> setValue(5)}/>
+        </div>
+    );
+};
