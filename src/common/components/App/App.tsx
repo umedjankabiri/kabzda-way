3d5d9ae02b9cb814f8efe925c4eff37da952def9
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 23:36:14 +0300

    Refactor: implemented attribute "title" for UncontrolledAccordion component, in the App component.The code has been separated, with h1 tag for Controlled and Uncontrolled components.

--- src/common/components/App/App.tsx
+++ src/common/components/App/App.tsx
@@ -3,19 +3,21 @@ import {Rating} from "common/components/Rating/Rating.tsx";
 import {Accordion} from "common/components/Accordion/Accordion.tsx";
 import {PageTitle} from "common/components/App/PageTitle.tsx";
 import {OnOff} from "common/components/OnOff/OnOff.tsx";
+import {UncontrolledAccordion} from "common/components/UncontrolledAccordion/UncontrolledAccordion.tsx";
 
 function App() {
     console.log("App rendering")
 
     return (
-        <div>
+        <div className={"App"}>
+            <h1>Controlled components</h1>
             <PageTitle title={'This is APP component'}/>
             <PageTitle title={'My friends'}/>
-            Article 1
+            <span>Article 1</span>
             <Rating value={3}/>
             <Accordion title={"Menu"} collapsed={true}/>
             <Accordion title={"Menu"} collapsed={false}/>
-            Article 2
+            <span>Article 2</span>
             <Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
@@ -23,6 +25,10 @@ function App() {
             <Rating value={4}/>
             <Rating value={5}/>
             <OnOff toggle={true}/>
+
+            <h1>Uncontrolled components</h1>
+            <UncontrolledAccordion title={"Menu"}/>
+
         </div>
     )
 }
