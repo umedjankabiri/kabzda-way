b892867eb23a57842512f88e6b040a753eefd07b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 19:25:27 +0300

    PageTitle component typed with PageTitleProps type

--- src/common/components/App/PageTitle.tsx
+++ src/common/components/App/PageTitle.tsx
@@ -1,4 +1,6 @@
-export const PageTitle = (props: any) => {
+import {PageTitleProps} from "common/types/PageTitle/PageTitleProps.ts";
+
+export const PageTitle = (props: PageTitleProps) => {
     console.log("PageTitle rendering")
 
     return <h2>{props.title}</h2>
