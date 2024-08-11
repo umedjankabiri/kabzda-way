6725d9a0e103865e12f1e7ace865ab0279f6e326
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 18:34:22 +0300

    Refactor: implemented attribute "title" for PageTitle component, in the App component

--- src/common/components/App/App.tsx
+++ src/common/components/App/App.tsx
@@ -1,17 +1,21 @@
 import 'common/components/App/App.css'
-import {AppTitle} from "common/components/App/AppTitle.tsx";
 import {Rating} from "common/components/Rating/Rating.tsx";
 import {Accordion} from "common/components/Accordion/Accordion.tsx";
+import {PageTitle} from "common/components/App/PageTitle.tsx";
 
 function App() {
     console.log("App rendering")
 
     return (
-        <>
-            <AppTitle/>
-            <Accordion/>
+        <div>
+            <PageTitle title={'This is APP component'}/>
+            <PageTitle title={'My friends'}/>
+            Article 1
             <Rating/>
-        </>
+            <Accordion/>
+            Article 2
+            <Rating/>
+        </div>
     )
 }
 
