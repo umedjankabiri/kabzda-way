1f67cce6a4e1d56b2d007d138b9f1102894513ec
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 23:24:40 +0300

    has been created UncontrolledStar component. The component typed with UncontrolledStarProps type, added function "onClickHandler" for event of click, implemented condition render in layout

---
+++ src/common/components/UncontrolledStar/UncontrolledStar.tsx
@@ -0,0 +1,12 @@
+import {UncontrolledStarProps} from "common/types/UncontrolledStar/UncontrolledStarProps.ts";
+
+export const UncontrolledStar = (props: UncontrolledStarProps) => {
+    console.log("UncontrolledStar rendering")
+    const onClickHandler = ()=> props.onClick && props.onClick()
+
+    return (
+        <span onClick={onClickHandler}>
+            {props.selected ? <b>Star </b> : "Star "}
+        </span>
+    )
+};
