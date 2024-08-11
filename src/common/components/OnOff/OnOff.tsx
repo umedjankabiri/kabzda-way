16148df6f9432fd2c98c58f85ab99472b51e2819
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-12 01:20:41 +0300

    Refactor: implemented "onClick" attributes for "div" tags, in the OnOff component, added two "onClickOnHandler" and "onClickOffHandler" functions, for toggle OnOff component

--- src/common/components/OnOff/OnOff.tsx
+++ src/common/components/OnOff/OnOff.tsx
@@ -2,16 +2,24 @@ import {OnOffProps} from "common/types/OnOff/OnOffProps.ts";
 import s from "common/components/OnOff/onOff.module.css"
 
 export const OnOff = (props: OnOffProps) => {
+    const onClickOnHandler = () => props.onClick && props.onClick(true)
+    const onClickOffHandler = () => props.onClick && props.onClick(false)
 
     return (
         <div className={s.mainWrapper}>
-            <div style={props.toggle ? {backgroundColor: "green"} : {backgroundColor: "white"}} className={s.on}>
+            <div className={s.on}
+                 style={props.toggle ? {backgroundColor: "green"} : {backgroundColor: "white"}}
+                 onClick={onClickOnHandler}
+            >
                 ON
+                <div className={s.off}
+                     style={!props.toggle ? {backgroundColor: "red"} : {backgroundColor: "white"}}
+                     onClick={onClickOffHandler}
+                >
+                    OFF
+                </div>
             </div>
-            <div style={!props.toggle ? {backgroundColor: "red"} : {backgroundColor: "white"}} className={s.off}>
-                OFF
-            </div>
-            <div style={props.toggle ? {backgroundColor: "green"} : {backgroundColor: "red"}} className={s.circle}>
+            <div className={s.circle} style={props.toggle ? {backgroundColor: "green"} : {backgroundColor: "red"}}>
 
             </div>
         </div>
