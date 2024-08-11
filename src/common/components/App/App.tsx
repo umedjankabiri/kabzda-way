dd63b7f160556cdb7572e871234fb1f3c4dcaee5
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 20:04:42 +0300

    Refactor: implemented attribute "toggle" for OnOff components, in the App component

--- src/common/components/App/App.tsx
+++ src/common/components/App/App.tsx
@@ -2,6 +2,7 @@ import 'common/components/App/App.css'
 import {Rating} from "common/components/Rating/Rating.tsx";
 import {Accordion} from "common/components/Accordion/Accordion.tsx";
 import {PageTitle} from "common/components/App/PageTitle.tsx";
+import {OnOff} from "common/components/OnOff/OnOff.tsx";
 
 function App() {
     console.log("App rendering")
@@ -21,6 +22,7 @@ function App() {
             <Rating value={3}/>
             <Rating value={4}/>
             <Rating value={5}/>
+            <OnOff toggle={true}/>
         </div>
     )
 }
