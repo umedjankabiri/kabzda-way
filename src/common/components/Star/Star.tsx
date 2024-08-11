7bd6aa81689ec2850fa145c3663d7c63e2fcf53d
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-12 00:54:11 +0300

    Refactor: implemented properties "onClick" and "value" for span tag, in the Star component

--- src/common/components/Star/Star.tsx
+++ src/common/components/Star/Star.tsx
@@ -4,7 +4,7 @@ export const Star = (props: StarProps) => {
     console.log("Star rendering")
 
     return (
-        <span>
+        <span onClick={()=> props.onClick && props.onClick(props.value)}>
             {props.selected ? <b>Star </b> : "Star "}
         </span>
     );
