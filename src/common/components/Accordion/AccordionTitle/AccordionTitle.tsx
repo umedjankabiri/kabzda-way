e4727fa53dc51afdafd9ccd1e297abe2120cddcd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 18:02:35 +0300

    added "title" property, with temporary type "any", for testing AccordionTitle component

--- src/common/components/Accordion/AccordionTitle/AccordionTitle.tsx
+++ src/common/components/Accordion/AccordionTitle/AccordionTitle.tsx
@@ -1,11 +1,7 @@
-export const AccordionTitle = () => {
+export const AccordionTitle = (props: any) => {
     console.log("AccordionTitle rendering")
 
     return (
-        <>
-            <h3>
-                Menu
-            </h3>
-        </>
+            <h3>{props.title}</h3>
     );
 };
