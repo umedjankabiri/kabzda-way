4641320531cc40b30f4c6a73f8592f9c3e11e1a0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 18:03:27 +0300

    Refactor: deleted empty tags, in the AccordionBody component

--- src/common/components/Accordion/AccordionBody/AccordionBody.tsx
+++ src/common/components/Accordion/AccordionBody/AccordionBody.tsx
@@ -2,12 +2,10 @@ export const AccordionBody = () => {
     console.log("AccordionBody rendering")
 
     return (
-        <>
             <ul>
                 <li>1</li>
                 <li>2</li>
                 <li>3</li>
             </ul>
-        </>
     );
 };
