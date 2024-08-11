a80056010d0c3000256f968110308718f0486e1c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-12 00:52:16 +0300

    Refactor: enhance StarProps type with "value" and "onClick" properties

--- src/common/types/Star/StarProps.ts
+++ src/common/types/Star/StarProps.ts
@@ -1,3 +1,7 @@
+import {RatingValueProps} from "common/types/Rating/RatingValueProps.ts";
+
 export type StarProps = {
+    value: RatingValueProps
     selected: boolean
+    onClick?: (value: RatingValueProps) => void
 }
