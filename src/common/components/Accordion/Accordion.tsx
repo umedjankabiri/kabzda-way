500ce57c27a9ac8ae2f4d075c4dfa0f5c768be1e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-11 19:34:31 +0300

    Accordion component typed with AccordionProps type

--- src/common/components/Accordion/Accordion.tsx
+++ src/common/components/Accordion/Accordion.tsx
@@ -1,7 +1,8 @@
 import {AccordionTitle} from "common/components/Accordion/AccordionTitle/AccordionTitle.tsx";
 import {AccordionBody} from "common/components/Accordion/AccordionBody/AccordionBody.tsx";
+import {AccordionProps} from "common/types/Accordion/AccordionProps.ts";
 
-export const Accordion = (props: any) => {
+export const Accordion = (props: AccordionProps) => {
     console.log("Accordion rendering")
 
     return (
