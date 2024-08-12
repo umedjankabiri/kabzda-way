6e7efbab249927bc15df083d2f2f9e9d7d7ac2de
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-12 21:49:25 +0300

    has been created DemonstrationOnOff component for demonstration behavior of the OnOff component, where property "toggle" controlled by hook useState

--- src/common/stories/OnOff/OnOff.stories.tsx
+++ src/common/stories/OnOff/OnOff.stories.tsx
@@ -1,5 +1,6 @@
 import {OnOff} from "common/components/OnOff/OnOff.tsx";
 import {Meta} from "@storybook/react";
+import {useState} from "react";
 
 const meta: Meta<typeof OnOff> = {
     title: 'OnOff stories',
@@ -14,3 +15,12 @@ export default meta
 
 export const switchedOn = () => <OnOff toggle={true} onClick={()=> {}}/>
 export const switchedOff = () => <OnOff toggle={false} onClick={()=> {}}/>
+export const DemonstrationOnOff = () => {
+    const [on, setOn] = useState<boolean>(false);
+
+    return (
+        <>
+            <OnOff toggle={on} onClick={setOn}/>
+        </>
+    )
+}
