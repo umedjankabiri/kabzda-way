c58668931419a38d029b1c8bfe75d2f77470378a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-26 07:22:16 +0300

    Refactor AccordionBody to use props and add dynamic rendering
    This commit updates the AccordionBody component to accept props and dynamically render items based on the provided data. The component now utilizes a callback function for item clicks to handle user interactions more effectively.

--- src/common/components/Accordion/AccordionBody/AccordionBody.tsx
+++ src/common/components/Accordion/AccordionBody/AccordionBody.tsx
@@ -1,11 +1,24 @@
-export const AccordionBody = () => {
+import {FC} from "react";
+import {AccordionBodyProps} from "common/types/Accordion/AccordionBodyProps.ts";
+
+export const AccordionBody: FC<AccordionBodyProps> = (props) => {
     console.log("AccordionBody rendering")
 
+    const mappedAccordionBody = props.items.map((item, index) => {
+        const onClickHandler = ()=> {
+            props.onClick && props.onClick(item.value)
+        }
+
+        return (
+            <li key={index} onClick={onClickHandler}>
+                {item.title}
+            </li>
+        )
+    })
+
     return (
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+        <ul>
+            {mappedAccordionBody}
+        </ul>
     );
 };
