44c862850fe17524e74249046570d2c2ce47abb4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-26 03:58:02 +0300

    Add UncontrolledRatingProps type definition
    Introduce the `UncontrolledRatingProps` type in the codebase, which includes `defaultValue` and `onClick` properties. This type will assist in managing the default value and click behavior for uncontrolled rating components.

---
+++ src/common/types/UncontrolledRating/UncontrolledRatingProps.ts
@@ -0,0 +1,6 @@
+import {RatingValueProps} from "common/types/Rating/RatingValueProps.ts";
+
+export type UncontrolledRatingProps = {
+    defaultValue?: RatingValueProps
+    onClick: (value: RatingValueProps) => void
+}
