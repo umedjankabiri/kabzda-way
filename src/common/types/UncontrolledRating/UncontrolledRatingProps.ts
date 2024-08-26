7363aa49e80ccd6d4b13d1a79c6e0418a2c3795d
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-26 04:01:01 +0300

    Make onClick prop optional in UncontrolledRatingProps
    The onClick prop in UncontrolledRatingProps is now optional, improving flexibility in component usage. This change allows consumers of the component to omit the onClick handler if it's not needed.

--- src/common/types/UncontrolledRating/UncontrolledRatingProps.ts
+++ src/common/types/UncontrolledRating/UncontrolledRatingProps.ts
@@ -2,5 +2,5 @@ import {RatingValueProps} from "common/types/Rating/RatingValueProps.ts";
 
 export type UncontrolledRatingProps = {
     defaultValue?: RatingValueProps
-    onClick: (value: RatingValueProps) => void
+    onClick?: (value: RatingValueProps) => void
 }
