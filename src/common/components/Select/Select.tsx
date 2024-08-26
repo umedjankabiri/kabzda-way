fbc4687ff941f1c8d4321a5494ed64b55149fd3a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-26 07:31:13 +0300

    Add Select component
    Introduced a new Select component with basic structure using TypeScript. The component currently accepts SelectProps but only renders an empty HTML select element. Further functionality and styling are yet to be implemented.

---
+++ src/common/components/Select/Select.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+import {SelectProps} from "common/types/Select/SelectProps.ts";
+
+export const Select: FC<SelectProps> = (props) => {
+
+
+    return (
+        <select>
+
+        </select>
+    );
+};
