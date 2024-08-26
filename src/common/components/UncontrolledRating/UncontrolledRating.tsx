f6558c3c17911c18b5a49fe336631bda01ad30de
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-26 03:58:47 +0300

    Add props to UncontrolledRating for default and onClick handling
    Modified UncontrolledRating to accept `defaultValue` and `onClick` props. This enhancement allows the component to initialize with a given rating and call a callback function when a star is clicked. This makes the component more flexible and reusable.

--- src/common/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/common/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,17 +1,53 @@
-import {useState} from "react";
+import {FC, useState} from "react";
 import {UncontrolledStar} from "common/components/UncontrolledStar/UncontrolledStar.tsx";
+import {UncontrolledRatingProps} from "common/types/UncontrolledRating/UncontrolledRatingProps.ts";
+import {RatingValueProps} from "common/types/Rating/RatingValueProps.ts";
 
-export const UncontrolledRating = () => {
+export const UncontrolledRating: FC<UncontrolledRatingProps> = (props) => {
     console.log("UncontrolledRating rendering")
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState<RatingValueProps>(props.defaultValue ? props.defaultValue : 0);
 
     return (
         <div>
-            <UncontrolledStar selected={value > 0} onClick={()=> setValue(1)}/>
-            <UncontrolledStar selected={value > 1} onClick={()=> setValue(2)}/>
-            <UncontrolledStar selected={value > 2} onClick={()=> setValue(3)}/>
-            <UncontrolledStar selected={value > 3} onClick={()=> setValue(4)}/>
-            <UncontrolledStar selected={value > 4} onClick={()=> setValue(5)}/>
+            <UncontrolledStar selected={value > 0}
+                              onClick={() => {
+                                  setValue(1);
+                                  props.onClick(1)
+                              }
+                              }
+            />
+            <UncontrolledStar selected={value > 1}
+                              onClick={
+                                  () => {
+                                      setValue(2);
+                                      props.onClick(2)
+                                  }
+                              }
+            />
+            <UncontrolledStar selected={value > 2}
+                              onClick={
+                                  () => {
+                                      setValue(3);
+                                      props.onClick(3)
+                                  }
+                              }
+            />
+            <UncontrolledStar selected={value > 3}
+                              onClick={
+                                  () => {
+                                      setValue(4);
+                                      props.onClick(4)
+                                  }
+                              }
+            />
+            <UncontrolledStar selected={value > 4}
+                              onClick={
+                                  () => {
+                                      setValue(5);
+                                      props.onClick(5)
+                                  }
+                              }
+            />
         </div>
     );
 };
