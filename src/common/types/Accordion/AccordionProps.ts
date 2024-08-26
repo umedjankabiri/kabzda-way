91c31c3f079c807e67b8217542e00df971d62f2b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-26 07:22:01 +0300

    Add items and onClickItems to AccordionProps
    Include a new property `items` with type `ItemProps[]` to represent items within the accordion. Also add `onClickItems` callback to handle item click events.

--- src/common/types/Accordion/AccordionProps.ts
+++ src/common/types/Accordion/AccordionProps.ts
@@ -1,5 +1,10 @@
+import {ItemProps} from "common/types/Accordion/ItemProps.ts";
+
 export type AccordionProps =  {
     title: string
     collapsed: boolean
+    items: ItemProps[]
     onClick?: (collapsed: boolean) => void
+    onClickItems?: (value: string | number | boolean) => void
+
 }
