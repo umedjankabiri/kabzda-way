a1b86641c2a3b3adcfdf2d759d75892765ed4610
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-05 18:18:04 +0300

    Add UncontrolledAccordionBody component
    Introduced a new component called UncontrolledAccordionBody. This component renders a simple unordered list with three list items.

---
+++ src/common/components/UncontrolledAccordionBody/UncontrolledAccordionBody.tsx
@@ -0,0 +1,9 @@
+export const UncontrolledAccordionBody = () => {
+    return (
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+    );
+};
