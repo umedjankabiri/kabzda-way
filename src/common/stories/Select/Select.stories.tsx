8603dd3d10e21b983e03bcd5d1415ab9a69dc963
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-05 13:01:34 +0300

    Add stories with and without initial value to Select component
    Introduce two new story variants for the `Select` component: one that initializes with a pre-selected value and one that starts with no value. This helps in demonstrating and testing the component behavior in different states.

--- src/common/stories/Select/Select.stories.tsx
+++ src/common/stories/Select/Select.stories.tsx
@@ -1,6 +1,47 @@
 import {Select} from "common/components/Select/Select.tsx";
+import {useState} from "react";
 
 export default {
     title: "Select stories",
     component: Select,
+}
+
+export const SelectWithValue = () => {
+    const [value, setValue] = useState('2')
+
+    const onChangeHandler = (newValue: string) => setValue(newValue)
+    return (
+        <>
+            <Select value={value}
+                    items={
+                        [
+                            {value: '1', title: "Minsk"},
+                            {value: '2', title: "Moscow"},
+                            {value: '3', title: "Kiev"}
+                        ]
+                    }
+                    onChange={onChangeHandler}
+            />
+        </>
+    )
+}
+
+export const SelectWithoutValue = () => {
+    const [value, setValue] = useState('')
+
+    const onChangeHandler = (newValue: string) => setValue(newValue)
+    return (
+        <>
+            <Select value={value}
+                    items={
+                        [
+                            {value: '1', title: "Minsk"},
+                            {value: '2', title: "Moscow"},
+                            {value: '3', title: "Kiev"}
+                        ]
+                    }
+                    onChange={onChangeHandler}
+            />
+        </>
+    )
 }
