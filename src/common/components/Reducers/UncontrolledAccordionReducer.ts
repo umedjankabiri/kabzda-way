6dd754a11a00e481bbea77819bdb87591435f470
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-05 19:37:43 +0300

    Refactor UncontrolledAccordionReducer to improve state handling
    Replaced Boolean state type with object to manage the 'collapsed' property and updated related action types and constants. This change brings more flexibility for future state enhancements in the UncontrolledAccordionReducer.

--- src/common/components/Reducers/UncontrolledAccordionReducer.ts
+++ src/common/components/Reducers/UncontrolledAccordionReducer.ts
@@ -1,11 +1,17 @@
-import {UncontrolledAccordionReducerProps} from "common/types/ReducersProps/UncontrolledAccordionReducerProps.ts";
+import {
+    InitialStateProps,
+    ActionProps
+} from "common/types/ReducersProps/ActionProps.ts";
 
-const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED"
+const TOGGLE_COLLAPSED = "TOGGLE-COLLAPSED"
 
-export const UncontrolledAccordionReducer = (state: boolean, action: UncontrolledAccordionReducerProps): boolean => {
+export const UncontrolledAccordionReducer = (state: InitialStateProps, action: ActionProps): InitialStateProps => {
     switch (action.type) {
-        case TOGGLE_CONSTANT:
-            return !state
+        case TOGGLE_COLLAPSED:
+            return {
+                ...state,
+                collapsed: !state.collapsed
+            }
         default:
             throw new Error("error in action type")
     }
