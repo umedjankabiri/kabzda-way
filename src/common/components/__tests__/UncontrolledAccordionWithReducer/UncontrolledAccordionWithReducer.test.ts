32b28d3d3cb5cf052aed351218120fa324dfb2dd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-05 19:39:13 +0300

    Add test for toggling collapsed state to false
    Added a new test case to ensure the collapsed state is set to false when the TOGGLE-COLLAPSED action is dispatched. This complements existing tests and improves coverage for the UncontrolledAccordionReducer component.

--- src/common/components/__tests__/UncontrolledAccordionWithReducer/UncontrolledAccordionWithReducer.test.ts
+++ src/common/components/__tests__/UncontrolledAccordionWithReducer/UncontrolledAccordionWithReducer.test.ts
@@ -11,4 +11,13 @@ test("collapsed should be true", () => {
 
     expect(newState.collapsed).toBe(true)
 })
+test("collapsed should be false", () => {
+    const state: InitialStateProps = {
+        collapsed: true
+    }
+    const action: ActionProps = {type: "TOGGLE-COLLAPSED"};
 
+    const newState = UncontrolledAccordionReducer(state, action)
+
+    expect(newState.collapsed).toBe(false)
+})
