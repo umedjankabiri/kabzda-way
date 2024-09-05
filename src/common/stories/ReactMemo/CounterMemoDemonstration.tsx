41c509b0201cd06a7cde572ef3e8693f88616e76
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-05 22:56:21 +0300

    Add CounterMemoDemonstration component
    Create a new component `MessagesCounterMemoDemonstration` to display a counter value. This component logs a message to the console and showcases a basic example of React memo usage.

---
+++ src/common/stories/ReactMemo/CounterMemoDemonstration.tsx
@@ -0,0 +1,10 @@
+
+export const MessagesCounterMemoDemonstration = (props: {counter: number})=> {
+    console.log("Messages counter memo demonstration");
+
+    return (
+        <div>
+            {props.counter}
+        </div>
+    )
+};
