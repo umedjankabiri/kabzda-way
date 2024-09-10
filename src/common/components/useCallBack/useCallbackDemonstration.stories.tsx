79a17b7bb67d00a87bbd0150f16e73dfba802786
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-10 20:53:18 +0300

    Add useCallback demonstration story
    Created a new story file for demonstrating the useCallback hook. This includes an example with `MessagesCounterMemoDemonstration` and `BooksWithUseCallbackDemonstration` components to show differences when using useMemo and useCallback.

---
+++ src/common/components/useCallBack/useCallbackDemonstration.stories.tsx
@@ -0,0 +1,37 @@
+import {memo, useCallback, useMemo, useState} from "react";
+import {MessagesCounterMemoDemonstration} from "common/stories/ReactMemo/CounterMemoDemonstration.tsx";
+import {BooksWithUseCallbackDemonstration} from "common/components/useCallBack/BooksWithUseCallbackDemonstration.tsx";
+
+export default {
+    title: 'useCallback',
+}
+
+const initialState: string[] = ["Learn JS/TS&React", "RTK toolkit", "Express.js"]
+
+export const useCallbackDemonstration = () => {
+    console.log("useCallback Demonstration")
+    const [counter, setCounter] = useState(0);
+    const [books, setBooks] = useState(initialState);
+
+    const memoizedAddBooksWithUseMemo = useMemo(()=> {
+        return () => {
+            console.log(books)
+            setBooks([...books, "Learn C++ " + new Date().getTime()]);
+        }
+    }, [books])
+    const memoizedAddBooksWithUseCallback = useCallback(()=> {
+            console.log(books)
+            setBooks([...books, "Learn C++ " + new Date().getTime()]);
+    }, [books])
+
+    return (
+        <div>
+            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <MessagesCounterMemoDemonstration counter={counter}/>
+            <span>addBook with useMemo: </span><NewBooksWithUseCallbackDemonstration addBook={memoizedAddBooksWithUseMemo}/>
+            <span>addBook with useCallback: </span><NewBooksWithUseCallbackDemonstration addBook={memoizedAddBooksWithUseCallback}/>
+        </div>
+    )
+}
+
+const NewBooksWithUseCallbackDemonstration = memo(BooksWithUseCallbackDemonstration)
