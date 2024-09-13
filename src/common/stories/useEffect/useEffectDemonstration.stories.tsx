b851f2f62fcbd711e4981c0ee25aedac4b16ca47
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-14 02:18:20 +0300

    Refactor useEffect hooks in demonstration component
    Splits the useEffect into three hooks to demonstrate different behaviors: on every render, on first render, and on dependency change. Adds a fake state to showcase multiple independent state updates.

--- src/common/stories/useEffect/useEffectDemonstration.stories.tsx
+++ src/common/stories/useEffect/useEffectDemonstration.stories.tsx
@@ -6,17 +6,32 @@ export default {
 
 export const UseEffectDemonstration = () => {
     console.log("useEffect Demonstration")
+    const [fake, setFake] = useState(1);
     const [counter, setCounter] = useState(1);
 
     useEffect(() => {
-        console.log("useEffect")
+        console.log("useEffect will be called every render")
         document.title = counter.toString();
     })
+    useEffect(() => {
+        console.log("useEffect will be called only in first render (ComponentDidMount)")
+        document.title = counter.toString();
+    }, [])
+    useEffect(() => {
+        console.log("useEffect will be called in first render, and when it's dependency will be changed")
+        document.title = counter.toString();
+    }, [counter])
 
     return (
         <div>
-            <button onClick={() => setCounter(counter + 1)}>+</button>
-            {counter}
+            <div>fake:
+                <button onClick={() => setFake(fake + 1)}>fake +</button>
+                {fake}
+            </div>
+            <div>counter:
+                <button onClick={() => setCounter(counter + 1)}>counter +</button>
+                {counter}
+            </div>
         </div>
     )
 }
