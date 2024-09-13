07f0d140a9521ab23791796da69f6e7d0d94eafe
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-14 02:39:06 +0300

    Add setInterval demonstration story
    Introduce a new story for demonstrating the use of setInterval in a React component. This includes state management and a useEffect hook to update a counter every interval.

---
+++ src/common/stories/setInterval/SetIntervalDemonstration.stories.tsx
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+
+export default {
+    title: 'setInterval stories',
+}
+
+export const SetIntervalDemonstration = () => {
+    console.log("setInterval Demonstration")
+    const [fake, setFake] = useState(1);
+    const [counter, setCounter] = useState(1);
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log("tick: " + counter);
+            setCounter(counter + 1);
+        })
+    }, [])
+
+    return (
+        <div>
+            <div>
+                fake: {fake}
+            </div>
+            <div>
+                counter: {counter}
+            </div>
+        </div>
+    )
+}
