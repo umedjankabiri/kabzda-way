78c6e41604f0e25ec6473839c51b3b08a9336bc8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-18 23:34:17 +0300

    Add Clock.stories.tsx to demonstrate Clock component
    Created a story for the Clock component in the Storybook. This will allow for easier visualization and testing of the Clock component's rendering.

---
+++ src/common/stories/Clock/Clock.stories.tsx
@@ -0,0 +1,12 @@
+import {Clock} from "common/components/Clock/Clock.tsx";
+
+export default {
+    title: "Clock stories",
+    component: Clock
+}
+
+export const ClockDemonstration = () => {
+    return (
+        <Clock />
+    )
+}
