af419bb0ebd7567fdb8458cb4d624dd8422f6982
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-18 23:34:36 +0300

    Add live clock update using useEffect
    Introduced a useEffect hook to update the current time every second. This change replaces the static time display with an auto-updating clock and ensures the time is formatted with leading zeros.

--- src/common/components/Clock/Clock.tsx
+++ src/common/components/Clock/Clock.tsx
@@ -1,16 +1,27 @@
-import {FC, useState} from "react";
+import {FC, useEffect, useState} from "react";
 import {ClockProps} from "common/types/OnOff/ClockProps.ts";
 
 export const Clock: FC<ClockProps> = (props) => {
     const [date, setDate] = useState(new Date())
 
+    useEffect(() => {
+        const timerID = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        return () => clearInterval(timerID);
+    }, []);
+
+    const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+    const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+    const seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+
     return (
         <div>
-            <span>{date.getHours()}</span>
+            <span>{hours}</span>
             :
-            <span>{date.getMinutes()}</span>
+            <span>{minutes}</span>
             :
-            <span>{date.getSeconds()}</span>
+            <span>{seconds}</span>
         </div>
     )
 }
